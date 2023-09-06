382cba603c8aa1a7272f1a3a61e68944116a5f9c
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-06 16:59:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let text = 'sample text, your test will appear here'
 const startBtn = document.getElementById('start')
 const container = document.getElementById('container')
 const textArea = document.querySelector('textarea')
+const charCounterTag = document.getElementById('char-counter')
+const mistakesTag = document.getElementById('mistakes-counter')
 
 function renderText(str) {
     const arr = str.split('')
@@ -35,9 +37,19 @@ startBtn.onclick = function(e) {
     document.querySelector('fieldset').style.display = 'none'
     text = textArea.value.trim() || renderDifficulty()
     renderText(text)
-    document.querySelector('h6').textContent = `Current Mistakes: ${mistakes}`
-    document.querySelector('h5').textContent = `${text.length} chars`
-    myInterval = setInterval(() => timer+= 0.25, 250)
+    mistakesTag.textContent = `Current Mistakes: ${mistakes}`
+    charCounterTag.textContent = `${text.length} chars`
+    myInterval = setInterval(() => {
+        timer+= 0.25; console.log(timer)
+    }, 250)
+}
+
+function convertSeconds(seconds) {
+    //example: 70secs = 1min and 10 secs
+    const mins = Math.round(seconds / 60)
+    const plural = mins === 1 ? '' : 's'
+    const secs = seconds % 60
+    return  `${mins} minute${plural}, ${secs}`
 }
 
 document.onkeypress = (e) => {
@@ -52,23 +64,28 @@ document.onkeypress = (e) => {
         nextChar?.classList?.add('hasBlink')
         char.classList?.remove('hasBlink')
     } else if (!char.dataset.tried) {
-
-        document.querySelector('h6').textContent=
+        mistakesTag.textContent=
         `Current Mistakes: ${++mistakes}`
         char.dataset.tried = true
         char.style.background = 'red'
         if (mistakes > Math.round(text.length * 0.25)) {
-            alert('TOO MANY MISTAKES, RESTARTED')
+            alert('TOO MANY MISTAKES! RESTARTED')
             window.location.reload()
             return
         }
     }
     if (currentCharCount === text.length) {
+        console.log(timer)
         clearInterval(myInterval)
         const correctCount = text.length - mistakes
         timer = timer < 0.25 ? 0.1 : timer // this is better than running the setInterval MORE times
         const wpm = [correctCount / timer * 60 ] / 4.7
-        console.log(timer)
+        if (timer > 60) {
+            console.log('it ran')
+             timer = convertSeconds(timer)
+             console.log('new timer', timer)
+        }
+
                     //average length of English words is 4.7 chars
         document.body.innerHTML = `<h1>You finished in ${timer} seconds</h1>
                                    <p>${mistakes} mistakes</p>
