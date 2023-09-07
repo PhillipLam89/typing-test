f0502d898e808b91c35600f90a02fa3b85083fad
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-07 15:00:39 -0700

    Update index2.js

--- index2.js
+++ index2.js
@@ -6,4 +6,43 @@ const collection = {
     ' This closed basin system is abnormal for most lakes. Water primarily leaves the lake by evaporating, resulting in upwards of 1,300 - 1,600 millimeters of evaporated water per year in this desert area! The result is that the waters of the Dead Sea have the highest salinity and density of any sea in the world, too high to support life. Because energy is required to break the bonds holding water molecules together evaporation removes heat from the environment, leading to a net cooling.' +
     "A watershed is an area of land that drains all the streams and rainfall to a common outlet such as the outflow of a reservoir, mouth of a bay, or any point along a stream channel . Watersheds can be as small as a footprint or large enough to encompass all the land that drains water into rivers that drain into Chesapeake Bay, where it enters the Atlantic Ocean. This map shows one set of watershed boundaries in the continental United States; these are known as National hydrologic units (watersheds)." +
     " Soil characteristics: In Georgia, clayey and rocky soils of the northern areas absorb less water at a slower rate than sandy soils, such as in Georgia's Coastal Plain. Soils absorbing less water results in more runoff overland into streams."
+
+}
+
+const row1 = [...'QWERTYUIOP']
+const row2 = [...'ASDFGHJKL']
+const row3 = [...'ZXCVBNM']
+const allRows = [row1, row2, row3]
+
+const board = document.getElementById('keyboard-container')
+function renderKeys(rowsArr) {
+  while (board.childElementCount < 3) {
+     const row = document.createElement('div')
+     row.id = `row${board.childElementCount}`
+     row.setAttribute('class', 'row')
+
+     for (let i = 0; i < rowsArr[board.childElementCount].length; i++) {
+        const tile = document.createElement('div')
+        tile.id =`${row.id}tile${i}`
+        tile.textContent = rowsArr[board.childElementCount][i]
+        tile.style.width = '2rem'
+        tile.style.height = '2rem'
+        tile.style.border = '2px solid royalblue'
+        tile.style.borderRadius= '50%'
+        tile.setAttribute('class', 'tile')
+        row.appendChild(tile)
+     }
+     board.appendChild(row)
+  }
+document.querySelector(`#row1`).style.margin = '1.65em'
+document.querySelector(`#row2`).style.marginLeft = '3.95em'
+}
+renderKeys(allRows)
+
+const allTiles = [...document.querySelectorAll('.tile')]
+
+function updateHighlights(letter) {
+  allTiles.forEach(tile => tile?.classList?.remove('hasHighlight'))
+  const newTarget = allTiles.find(tile => tile.textContent == letter.toUpperCase())
+  newTarget?.classList?.add('hasHighlight')
 }
